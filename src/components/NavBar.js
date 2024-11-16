586d5132d7d7faaa5372fc7fcddef2fbbe8a818e
= <=>
2024-11-16 06:21:34 +0545

    nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMenu, IoMoon } from "react-icons/io5";
+import { IoSunny } from "react-icons/io5";
+import { IoIosArrowUp } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const isOpenStyle = isOpen
+    ? "flex items-center flex-wrap justify-center space-x-8"
+    : "flex items-center space-x-8";
+
+  const isOpenStyleNav = isOpen
+    ? "flex flex-col justify-center items-center gap-x-9 z-50 md:hidden w-full justify-center my-3"
+    : "flex flex-row gap-x-10 hidden md:block";
+
+  return (
+    <header className="top-0 flex justify-center items-center py-5 w-full text-lg bg-opacity-50 border-b border-black">
+      <div className={isOpenStyle}>
+        <a href="https://www.linkedin.com/in/sushant-tripathee-94a753280/">
+          <img
+            className="rounded-full size-14 transform transition-all duration-200 hover:scale-110 md:size-12"
+            src="https://i.imgur.com/SoEuNHn.png"
+            alt="sushant"
+          />
+        </a>
+
+        <h1 className="md:hidden text-xl font-bold">Sushant Tripathee</h1>
+        <button
+          className="md:hidden text-3xl"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <IoMenu />
+        </button>
+
+        <nav className={isOpenStyleNav}>
+          <a className="hover:underline hover:text-[#df5e4d] mx-5">Home</a>
+          <a className="hover:underline hover:text-[#df5e4d] mx-5">Skills</a>
+          <a className="hover:underline hover:text-[#df5e4d] mx-5">Projects</a>
+          <a className="hover:underline hover:text-[#df5e4d] mx-5">Contacts</a>
+        </nav>
+
+        <button className="text-2xl">
+          <IoMoon />
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
